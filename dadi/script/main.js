21fbabd9513bc3cfc7000111e5e485f51be57412
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-02-27 14:42:43 +0100

    feat: add solution for dadi exercize

--- dadi/script/main.js
+++ dadi/script/main.js
@@ -2,3 +2,25 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 // Bonus
 // Invece di generare il numero random per l'utente, chiedilo con un prompt!
+
+// user dice genrator
+const userDice = Math.floor(Math.random() * 6 ) + 1;
+// computer dice genrator
+const computerDice = Math.floor(Math.random() * 6 ) + 1;
+
+//print userDice and computerDice
+console.log('User dice:', userDice);
+console.log('Computer dice:', computerDice);
+
+if (userDice > computerDice) {
+
+    console.log('You win!');
+}
+else if (computerDice > userDice) {
+
+    console.log('You lose!');
+}
+else {
+
+    console.log('It\'s a tie!');
+}
